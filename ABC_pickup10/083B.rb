8460df9f3ff09d7632bafb681062b1e51e804a17
mochikichi <mochikichi@live.jp>
2020-06-25 08:51:31 +0900

    modify 083B

--- ABC_pickup10/083B.rb
+++ ABC_pickup10/083B.rb
@@ -1,10 +1,19 @@
 #https://atcoder.jp/contests/abc083/tasks/abc083_b
 
 N, A, B = gets.chomp.split.map(&:to_i)
-nums = (1..N).map{ |n| n.to_s.split('').map(&:to_i).inject(:+) }
+
+def digit_sum(n)
+  sum = 0
+  while (n > 0) do
+    sum += n % 10
+    n /= 10
+  end
+  sum
+end
 
 result = []
-nums.each_with_index do |n, i|
-  result << i + 1 if A <= n && n <= B
+(1..N).each_with_index do |n, i|
+  s = digit_sum(n)
+  result << i + 1 if A <= s && s <= B
 end
-puts result.inject(:+)
+puts result.sum
